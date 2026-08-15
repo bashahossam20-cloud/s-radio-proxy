7e304b4c9d709d6ccaf602e6e977a23562d1180a
bashahossam20-cloud <basha.hossam20@gmail.com>
2026-08-16 00:14:28 +0300

    Update stream.js

--- api/stream.js
+++ api/stream.js
@@ -1,27 +1,27 @@
 export const config = {
-runtime: 'edge',
+  runtime: 'edge',
 };
 
 export default async function handler(req) {
-const streamUrl = "http://dc5.serverse.com/proxy/pbmhbvxs/stream
-";
+  // الرابط الجديد
+  const streamUrl = "http://dc5.serverse.com/proxy/pbmhbvxs/stream";
 
-try {
-const response = await fetch(streamUrl, {
-headers: {
-"User-Agent": "Mozilla/5.0",
-},
-});
+  try {
+    const response = await fetch(streamUrl, {
+      headers: {
+        "User-Agent": "Mozilla/5.0",
+      },
+    });
 
-return new Response(response.body, {
-status: response.status,
-headers: {
-"Access-Control-Allow-Origin": "*",
-"Content-Type": "audio/mpeg",
-"Cache-Control": "no-cache, no-store",
-},
-});
-} catch (error) {
-return new Response("خطأ في جلب البث", { status: 502 });
-}
+    return new Response(response.body, {
+      status: response.status,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "audio/mpeg",
+        "Cache-Control": "no-cache, no-store",
+      },
+    });
+  } catch (error) {
+    return new Response("خطأ في جلب البث", { status: 502 });
+  }
 }
